c4f3945b886bfc4ba628ea49f66e3251e9f6f5c0
Chenxi Liu <chenxi_liu@live.cn>
2017-07-19 11:31:08 -0400

    add LICENSE

--- caffemodel2npy.py
+++ caffemodel2npy.py
@@ -1,10 +1,29 @@
-import numpy as np
-import pdb
-import re
+# Copyright 2017 Chenxi Liu. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+# This code is modified from 
+# https://github.com/ppwwyyxx/tensorpack/blob/master/tensorpack/utils/loadcaffe.py
 
 # sample usage:
 # python caffemodel2npy.py deploy.prototxt 
 # ../deeplab/ResNet101/init.caffemodel ./model/ResNet101_init.npy
+
+import numpy as np
+import pdb
+import re
+
 
 class CaffeLayerProcessor(object):
     def __init__(self, net):
--- deeplab_model.py
+++ deeplab_model.py
@@ -1,4 +1,4 @@
-# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2017 Chenxi Liu. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
+# This code is modified from 
+# https://github.com/tensorflow/models/blob/master/resnet/resnet_model.py
 
 """DeepLab model.
 
